ff5aa60d7fc06d3732220fbb8b70d0c499bbeb02
Alex N <nikitin2009@gmail.com>
2018-12-17 12:25:58 +0200

    Updates

---
+++ precache-manifest.ccc2168ad33104f3d8f8d184cb3b6c25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33d495bffc2fe98bffff",
+    "url": "/star-db/static/css/main.c4763cf6.chunk.css"
+  },
+  {
+    "revision": "33d495bffc2fe98bffff",
+    "url": "/star-db/static/js/main.33d495bf.chunk.js"
+  },
+  {
+    "revision": "7b6b7cf3c73cb2fa1ab3",
+    "url": "/star-db/static/js/1.7b6b7cf3.chunk.js"
+  },
+  {
+    "revision": "efac5df8a606d470b452",
+    "url": "/star-db/static/js/runtime~main.efac5df8.js"
+  },
+  {
+    "revision": "b822187f23b90b579c0767c5fbb15484",
+    "url": "/star-db/index.html"
+  }
+];
